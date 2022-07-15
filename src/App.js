e6cfe84677578b035793af08f96e3a26a8ea87de
iraand <iraand@bigmir.net>
2022-07-15 03:06:36 +0300

    build app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { lazy, Suspense } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+import CircleLoader from "react-spinners/ClipLoader";
+
+import './scss/main.scss';
+import Home from './pages/home'
+import {Navbar} from './components/Navbar'
+
+
+
+const Voting = lazy(() => import ('./pages/voting'));
+const Breeds = lazy(() => import ('./pages/breeds'));
+const Breed = lazy(() => import ('./pages/breed'));
+const Gallery = lazy(() => import ('./pages/gallery'));
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className='container'>    
+      <Router> 
+        <Navbar />       
+        <Suspense fallback={<CircleLoader speedMultiplier={1} color={"#FF868E"} size={150} className="loader"/>}>           
+          <Routes>           
+            <Route exact path={'/'} element={<Home />} ></Route>
+            <Route path={'/voting'} element={<Voting />} ></Route>
+            <Route path={'/breeds'} element={<Breeds />} ></Route>
+            <Route path={'/breed/:id'} element={<Breed />} ></Route>
+            <Route path={'/gallery'} element={<Gallery />} ></Route>          
+          </Routes>
+        </Suspense>
+      </Router>                   
+    </div>   
   );
 }
 
